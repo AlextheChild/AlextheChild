c37cba7c5405d1bac87278b3fa90958ecc493490
AlextheChild <alexjun2008@gmail.com>
2024-07-14 22:53:00 -0400

    lmao

--- python/school/balloon_popper_cnn/utils/flip_boxes.py
+++ python/school/balloon_popper_cnn/utils/flip_boxes.py
@@ -1,11 +1,22 @@
-rows = open("cnn/data/test/test.csv").read().strip().split("\n")
+train_rows = open("cnn/data/train/train.csv").read().strip().split("\n")
+test_rows = open("cnn/data/test/test.csv").read().strip().split("\n")
+
+rows = train_rows
+
 for row in rows:
     row = row.split(", ")
+
+    path = row[0]
+    x = int(row[1])
+    y = int(row[2])
+    w = int(row[3])
+    h = int(row[4])
+
     print(
         "flipped_" + row[0],
-        2880 - int(row[3]),
-        row[2],
-        2880 - int(row[1]),
-        row[4],
+        2880 - x - w,
+        y,
+        w,
+        h,
         sep=", ",
     )
