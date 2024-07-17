8a70e398aa9f8d7ea52aed5da093bbd602b6f0f3
AlextheChild <alexjun2008@gmail.com>
2024-07-17 03:34:39 -0400

    pipeline complete

--- python/school/balloon_popper_cnn/gui/main.py
+++ python/school/balloon_popper_cnn/gui/main.py
@@ -10,6 +10,8 @@ import display as d
 ! cv2 color masking
 ! there are a lot of magic variables/numbers in the gui code so maybe change that
 ! make framerate better
+! clean up global variables
+! yolo type network
 """
 
 
