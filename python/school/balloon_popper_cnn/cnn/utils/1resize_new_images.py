4323ba3d10e0388492f11839f7f5fbd2f285fd07
AlextheChild <alexjun2008@gmail.com>
2024-07-17 01:46:17 -0400

    epic pipeline

--- python/school/balloon_popper_cnn/cnn/utils/1resize_new_images.py
+++ python/school/balloon_popper_cnn/cnn/utils/1resize_new_images.py
@@ -6,4 +6,4 @@ sender_directory = os.listdir("cnn/data/new_images")
 for file in sender_directory:
     image = Image.open("cnn/data/new_images/" + file)
     image = image.resize((2880, 1800))
-    image.save(file)
+    image.save("cnn/data/new_images/" + file)
