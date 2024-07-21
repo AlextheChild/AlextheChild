31955726d5b97e8a306be2993c6fd766c079c496
AlextheChild <alexjun2008@gmail.com>
2024-07-21 00:11:27 -0400

    lmao I changed my mind

--- python/school/balloon_popper_cnn/models/cnn/utils/1resize_new_images.py
+++
@@ -1,9 +0,0 @@
-import os
-from PIL import Image
-
-sender_directory = os.listdir("models/cnn/data/new_images")
-
-for file in sender_directory:
-    image = Image.open("models/cnn/data/new_images/" + file)
-    image = image.resize((2880, 1800))
-    image.save("models/cnn/data/new_images/" + file)
--- python/school/balloon_popper_cnn/models/cnn/utils/2flip_images.py
+++
@@ -1,9 +0,0 @@
-import os
-from PIL import Image
-
-directory = os.listdir("models/cnn/data/images")
-
-for file in directory:
-    image = Image.open("models/cnn/data/images/" + file)
-    image = image.transpose(Image.FLIP_LEFT_RIGHT)
-    image.save("models/cnn/data/flipped_images/" + file)
